900e7a59fad8c54935a0ce3283cda93bfef8a5e2
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-05-20 17:55:06 -0600

    Update User model and spec with appropriate relationship information
    Co-authored-by: Patrick Duvall <duvallp208@gmail.com>
    Co-authored-by: Kyle Cornelissen <kyle.cornelissen@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
+  has_many :items
   has_many :orders
-  has_many :order_items, through: :orders
-  has_many :items, through: :order_items
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "relationships" do
+    it {should have_many(:items)}
     it {should have_many :orders}
-    it {should have_many(:order_items).through(:orders)}
-    it {should have_many(:items).through(:order_items)}
   end
 end
