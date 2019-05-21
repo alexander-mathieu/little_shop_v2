6098695164afa9468d3fbebd3b8455b57e7bae72
Aurie <Myrdden42@gmail.com>
2019-05-21 17:02:59 -0600

    Fix spoof user in test

--- spec/features/session/login_spec.rb
+++ spec/features/session/login_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe "As a visitor," do
   describe "I can login on the welcome page" do
     it "and it works I guess" do
-      user = User.create!(email: "help@gmail.com", password: "12345")
+      user = User.create!(email: "bob@bob.com", password: "124355",
+        name: "bob", address:"123 bob st.", city: "bobton", state:"MA", zip: 28234)
 
       visit root_path
 
