7e2cd0478b0dc7c9d3343c43a7e1aa0251eb1822
Patrick Duvall <duvallp208@gmail.com>
2019-05-21 12:37:50 -0600

    Add render login unless current user to user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    render file: "app/views/users/new.html.erb" if current_user.nil?
     @user = User.find(params[:id])
   end
 end
