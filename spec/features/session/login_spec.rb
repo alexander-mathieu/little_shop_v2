0bbf0909d096bbfa7a628ab5bce64088a2b80763
Aurie <Myrdden42@gmail.com>
2019-05-20 21:34:58 -0600

    Fixed spec

--- spec/features/session/login_spec.rb
+++ spec/features/session/login_spec.rb
@@ -2,18 +2,20 @@ require 'rails_helper'
 
 RSpec.describe "As a visitor, " do
   describe "I can login on the welcome page" do
-    user = User.create(email: "help@gmail.com", password: "12345")
+    it "can login" do
+      user = User.create(email: "help@gmail.com", password: "12345")
 
-    visit root_path
+      visit root_path
 
-    click_on "login?"
+      click_on "login?"
 
-    expect(current_path).to eq(login_path)
-    fill_in "email", with: user.email
-    fill_in "password", with: user.password
+      expect(current_path).to eq(login_path)
+      fill_in "email", with: user.email
+      fill_in "password", with: user.password
 
-    click_on "Log In"
+      click_on "Log In"
 
-    expect(current_path).to eq(user_path(user))
+      expect(current_path).to eq(user_path(user))
+    end
   end
 end
