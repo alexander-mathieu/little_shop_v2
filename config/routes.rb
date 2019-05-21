851368259e7f72d98093605de82965311944ddd7
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-05-20 19:24:21 -0600

    Add new user and login routes
    Co-authored-by: Patrick Duvall <duvallp208@gmail.com>
    Co-authored-by: Kyle Cornelissen <kyle.cornelissen@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
 
   resources :items, only: [:index]
-  
   resources :merchants, only: [:index]
+  resources :users, only: [:new]
+
+  get '/login', to: 'sessions#new'
 end
