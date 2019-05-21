9ab13f85e08d8b1ada101fa84ac0f1792fb3da05
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-05-21 15:36:19 -0600

    Add new and create methods to Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,24 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      flash[:message] = "Welcome, #{user.name}. You are now registered and logged in!"
+      # change path to profile_path
+      redirect_to root_path
+    else
+      @user = User.new(user_params)
+      flash[:error] = "You are missing required fields, your email is already in use, or your passwords don't match."
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :address, :city, :state, :zip, :email, :password, :password_confirmation)
   end
 end
