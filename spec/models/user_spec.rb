97e878ab82653dd60caba0924d9be35885b17692
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-05-22 16:24:53 -0600

    Add user model test for find_merchants class method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,17 @@ RSpec.describe User, type: :model do
     it {should have_many(:items)}
     it {should have_many :orders}
   end
+
+  describe "class methods" do
+    before :each do
+      @user_1 = create(:merchant)
+      @user_2 = create(:user)
+      @user_3 = create(:merchant)
+
+      @users = User.all
+    end
+    it ".find_merchants" do
+      expect(@users.find_merchants).to eq([@user_1, @user_3])
+    end
+  end
 end
