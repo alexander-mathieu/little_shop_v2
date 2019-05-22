b944629f4bd184ab7e081c544854b5193538efba
Aurie <Myrdden42@gmail.com>
2019-05-22 13:41:06 -0600

    Add cart add route

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  post '/cart', to: 'carts#create'
   get '/cart', to: 'carts#show'
   delete '/logout', to: "sessions#delete"
 end
