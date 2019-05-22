6f58ac384f0134a0182b1fd67fe96a361cd92955
Patrick Duvall <duvallp208@gmail.com>
2019-05-22 07:48:09 -0600

    Resolve item validation in item show test

--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe "as any kind of user" do
   describe "when I visit an item's show page" do
     before :each do
-      @user_1 = User.create!(email: "user_1@gmail.com", role: 2, name: "User 1", address: "User 1 Address", city: "User 1 City", state: "User 1 State", zip: "12345")
-      @user_2 = User.create!(email: "user_2@gmail.com", role: 2, name: "User 2", address: "User 2 Address", city: "User 1 City", state: "User 1 State", zip: "22345")
+      @user_1 = User.create!(email: "user_1@gmail.com", role: 2, name: "User 1", address: "User 1 Address", city: "User 1 City", state: "User 1 State", zip: "12345", password: "123456")
+      @user_2 = User.create!(email: "user_2@gmail.com", role: 2, name: "User 2", address: "User 2 Address", city: "User 1 City", state: "User 1 State", zip: "22345", password: "1233356")
 
       @item_1 = @user_1.items.create!(name: "Item 1", price: 1.00, description: "Item 1 Description", image: "https://www.warrenhannonjeweler.com/static/images/temp-inventory-landing.jpg", inventory: 10)
       @item_2 = @user_2.items.create!(name: "Item 2", price: 1.50, description: "Item 2 Description", image: "https:/tradersofafrica.com/img/no-product-photo.jpg", inventory: 15)
