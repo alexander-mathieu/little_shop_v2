9f98e8e9bb4df85662cc76128ebd22d19366ec3a
Aurie <Myrdden42@gmail.com>
2019-05-21 18:19:42 -0600

    Add redirect framework

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,23 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to user_path(user)
+    if @current_user
+      if @current_user.admin?
+        #redirect_to admin_dash_path(@current_user)
+      elsif @current_user.merchant?
+        #redirect_to merch_dash_path(@current_user)
+      else
+        redirect_to user_path(@current_user)
+      end
     else
-      flash[:warn] = "Some of your information isn't correct."
-      render :new
+      user = User.find_by(email: params[:email])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect_to user_path(user)
+      else
+        flash[:warn] = "Some of your information isn't correct."
+        render :new
+      end
     end
   end
 end
