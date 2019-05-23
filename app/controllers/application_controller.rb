9ea8bd58dfa4567a6f756c2ff74a68c25f43a6e9
Patrick Duvall <duvallp208@gmail.com>
2019-05-23 07:53:30 -0600

    Refactor to use native merchant? enum method and add merchant show route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,4 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-
-  def user_is_merchant?
-     current_user.role == 1
-   end
 end
