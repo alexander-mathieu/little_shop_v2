6e9a0639256f7b5ba88caf3b1bece4272cfc94c3
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-05-24 12:15:54 -0600

    Add spec for Admin not seeing navigation links for cart

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -2,33 +2,39 @@ require 'rails_helper'
 
 ###Tests for navbar showing number of items in cart are in cart_show reature spec
 
-describe "as a visitor" do
-  describe "when I visit the welcome page" do
+RSpec.describe "when I visit the welcome page" do
+  context "as a visitor" do
     it "it displays a navigation bar" do
       visit root_path
 
       within ".navbar" do
         click_link("Home")
         expect(current_path).to eq(root_path)
+
         click_link("Items")
         expect(current_path).to eq(items_path)
+
         click_link("Merchants")
         expect(current_path).to eq(merchants_path)
+
         click_link("Cart")
         expect(current_path).to eq(cart_path)
+
         click_link("LogIn")
         expect(current_path).to eq(login_path)
+
         click_link("Register")
         expect(current_path).to eq(register_path)
       end
     end
   end
 
-describe "as a registered user" do
+  context "as a registered user" do
     it "lets a user log out" do
-      user = User.create!(email: "bob@bob.com", password: "124355",
-        name: "bob", address:"123 bob st.", city: "bobton", state:"MA", zip: 28234)
+      user = User.create!(email: "bob@bob.com", password: "124355", name: "bob", address:"123 bob st.", city: "bobton", state:"MA", zip: 28234)
+
       visit root_path
+
       click_on "LogIn"
       fill_in "email", with: user.email
       fill_in "password", with: user.password
@@ -36,32 +42,40 @@ describe "as a registered user" do
 
       within ".navbar" do
         click_link("Logout")
+
         expect(current_path).to eq(root_path)
       end
       expect(page).to have_content("You are now logged Out")
     end
 
-      it "gives me different options as a user" do
-        user = User.create!(email: "bob@bob.com", password: "124355",
-          name: "bob", address:"123 bob st.", city: "bobton", state:"MA", zip: 28234)
-        visit root_path
-        click_on "LogIn"
-        fill_in "email", with: user.email
-        fill_in "password", with: user.password
-        click_on "Log In"
-        within ".navbar" do
+    it "gives me different options as a user" do
+      user = User.create!(email: "bob@bob.com", password: "124355", name: "bob", address:"123 bob st.", city: "bobton", state:"MA", zip: 28234)
+
+      visit root_path
+
+      click_on "LogIn"
+      fill_in "email", with: user.email
+      fill_in "password", with: user.password
+      click_on "Log In"
+
+      within ".navbar" do
         click_link("Home")
         expect(current_path).to eq(root_path)
+
         click_link("Items")
         expect(current_path).to eq(items_path)
+
         click_link("Merchants")
         expect(current_path).to eq(merchants_path)
+
         click_link("Cart")
         expect(current_path).to eq(cart_path)
         expect(page).to_not have_link("LogIn")
         expect(page).to_not have_link("Register")
+
         click_link("Profile")
         expect(current_path).to eq(profile_path)
+
         click_link("Logout")
         expect(current_path).to eq(root_path)
         expect(page).to have_link("LogIn")
@@ -70,53 +84,85 @@ describe "as a registered user" do
     end
 
     it "directs to a users profile if already logged in" do
-      user = User.create!(email: "bob@bob.com", password: "124355",
-        name: "bob", address:"123 bob st.", city: "bobton", state:"MA", zip: 28234)
+      user = User.create!(email: "bob@bob.com", password: "124355", name: "bob", address:"123 bob st.", city: "bobton", state:"MA", zip: 28234)
+
       visit root_path
+
       click_on "LogIn"
       fill_in "email", with: user.email
       fill_in "password", with: user.password
       click_on "Log In"
       visit "/login"
+
       expect(current_path).to eq(profile_path)
     end
   end
-end
 
-  describe "as a merchant" do
+  context "as a merchant" do
     before :each do
       @merchant = User.create!(email: "joe@joe.com", password: "124355",
         name: "joe", role: 1, address:"123 joe st.", city: "joeton", state:"MI", zip: 28334)
-        visit root_path
-        click_on "LogIn"
-        fill_in "email", with: @merchant.email
-        fill_in "password", with: @merchant.password
-        click_on "Log In"
+
+      visit root_path
+
+      click_on "LogIn"
+      fill_in "email", with: @merchant.email
+      fill_in "password", with: @merchant.password
+      click_on "Log In"
     end
+
     it "shows me the same links as a visitor" do
       click_link("Home")
       expect(current_path).to eq(root_path)
+
       click_link("Items")
       expect(current_path).to eq(items_path)
+
       click_link("Merchants")
       expect(current_path).to eq(merchants_path)
     end
 
     it "also shows the logout and dashboard" do
-        visit root_path
+      visit root_path
+
       click_link("Dashboard")
       expect(current_path).to eq(dashboard_path)
+
       click_link("Logout")
       expect(current_path).to eq(root_path)
     end
 
     it "doesnt show login/register or cart" do
       visit root_path
+
       expect(page).to_not have_link("LogIn")
       expect(page).to_not have_link("Register")
       expect(page).to_not have_link("Cart")
     end
   end
+
+  context "as an admin" do
+    before :each do
+      @admin = User.create!(email: "admin@gmail.com", password: "124356", name: "Admin", role: 2, address: "Admin Address", city: "Admin City", state: "MA", zip: 28334)
+
+      visit root_path
+
+      click_on "LogIn"
+      fill_in "email", with: @admin.email
+      fill_in "password", with: @admin.password
+      click_on "Log In"
+    end
+
+    it "the page doesn't display a link to my cart or item count" do
+      visit root_path
+
+      within ".navbar" do
+        expect(page).to_not have_link("Cart")
+        expect(page).to_not have_content("Items in cart")
+      end
+    end
+  end
+end
 
 #   As a merchant user
 # I see the same links as a visitor
