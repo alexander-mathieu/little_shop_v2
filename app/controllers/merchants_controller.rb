41f96462d693a7e762e50f4b88a99b1bf539dba2
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-05-24 17:27:17 -0600

    Add controller methods for merchants statistics

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,11 +1,17 @@
 class MerchantsController < ApplicationController
   def index
     @merchants = User.find_merchants
+    @merchants_top_three_revenue = User.top_three_revenue
+    @merchants_quickest_fulfillments = User.top_three_fulfillments("asc")
+    @merchants_slowest_fulfillments = User.top_three_fulfillments("desc")
+    @merchants_top_three_states = User.top_three_orders_by("state")
+    @merchants_top_three_cities = User.top_three_orders_by("city")
+    @top_three_orders = Order.top_three_order_item_quantity
   end
 
   def show
     @user = current_user
     render file: "app/public/404.html" unless @user.merchant?
-    
+
   end
 end
