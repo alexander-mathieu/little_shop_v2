061b56e34aafc3ba14da03637bfcf5fda095ecfe
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-05-24 17:24:16 -0600

    Add top_three_order_item_quantity class method to order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,12 @@ class Order < ApplicationRecord
   has_many :items, through: :order_items
 
   enum status: ["pending", "packaged", "shipped", "cancelled"]
+
+  def self.top_three_order_item_quantity
+    joins(:order_items)
+          .select('orders.*', 'SUM(order_items.quantity) AS order_item_quantity')
+          .group('orders.id')
+          .order('order_item_quantity desc')
+          .limit(3)
+  end
 end
