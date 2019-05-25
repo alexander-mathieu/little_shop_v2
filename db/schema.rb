418b2165197f36a3650656343810749038a0df60
Aurie <Myrdden42@gmail.com>
2019-05-25 17:23:24 -0600

    Give active default value

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20190520225214) do
   create_table "items", force: :cascade do |t|
     t.bigint "user_id"
     t.string "name"
-    t.boolean "active"
+    t.boolean "active", default: true
     t.decimal "price"
     t.text "description"
     t.string "image"
