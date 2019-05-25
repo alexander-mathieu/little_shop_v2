bc8f95e1376b5ad9a48b4184675ebd332134b6eb
Aurie <Myrdden42@gmail.com>
2019-05-25 16:06:29 -0600

    Add items to merchant show

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -11,7 +11,7 @@ class MerchantsController < ApplicationController
 
   def show
     @user = current_user
-    render file: "app/public/404.html" unless @user.merchant?
+    @items = 
 
   end
 end
