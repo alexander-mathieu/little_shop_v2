08d11c4cf39c5d156dda079924216e10b3ded2ef
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-05-25 09:52:40 -0600

    Add spec for Admin seeing User join dates on Admin User index

--- spec/features/admin/users/index_spec.rb
+++ spec/features/admin/users/index_spec.rb
@@ -7,10 +7,10 @@ RSpec.describe "as an admin" do
       @admin_2 = User.create!(email: "admin_2@gmail.com", role: 2, name: "Admin 2", address: "Admin 2 Address", city: "Admin 2 City", state: "Admin 2 State", zip: "22345", password: "123456")
       @merchant_1 = User.create!(email: "merchant_1@gmail.com", role: 1, name: "Merchant 1", address: "Merchant 1 Address", city: "Merchant 1 City", state: "Merchant 1 State", zip: "32345", password: "123456")
       @merchant_2 = User.create!(email: "merchant_2@gmail.com", role: 1, name: "Merchant 2", address: "Merchant 2 Address", city: "Merchant 2 City", state: "Merchant 2 State", zip: "42345", password: "123456")
-      @user_1 = User.create!(email: "user_1@gmail.com", role: 0, name: "User 1", address: "User 1 Address", city: "User 1 City", state: "User 1 State", zip: "52345", password: "123456")
-      @user_2 = User.create!(email: "user_2@gmail.com", role: 0, name: "User 2", address: "User 2 Address", city: "User 2 City", state: "User 2 State", zip: "62345", password: "123456")
-      @user_3 = User.create!(email: "user_3@gmail.com", role: 0, name: "User 3", address: "User 3 Address", city: "User 3 City", state: "User 3 State", zip: "72345", password: "123456")
-      @user_4 = User.create!(email: "user_4@gmail.com", role: 0, name: "User 4", address: "User 4 Address", city: "User 4 City", state: "User 4 State", zip: "82345", password: "123456")
+      @user_1 = User.create!(email: "user_1@gmail.com", role: 0, name: "User 1", address: "User 1 Address", city: "User 1 City", state: "User 1 State", zip: "52345", password: "123456", created_at: 4.days.ago)
+      @user_2 = User.create!(email: "user_2@gmail.com", role: 0, name: "User 2", address: "User 2 Address", city: "User 2 City", state: "User 2 State", zip: "62345", password: "123456", created_at: 3.days.ago)
+      @user_3 = User.create!(email: "user_3@gmail.com", role: 0, name: "User 3", address: "User 3 Address", city: "User 3 City", state: "User 3 State", zip: "72345", password: "123456", created_at: 2.days.ago)
+      @user_4 = User.create!(email: "user_4@gmail.com", role: 0, name: "User 4", address: "User 4 Address", city: "User 4 City", state: "User 4 State", zip: "82345", password: "123456", created_at: 1.days.ago)
 
       visit root_path
 
@@ -33,7 +33,7 @@ RSpec.describe "as an admin" do
 
     it "I see each user's name as a link to a show page for that user" do
       visit admin_users_path
-      
+
       within ".all-default-users" do
         expect(page).to have_link(@user_1.name)
         expect(page).to have_link(@user_2.name)
@@ -42,12 +42,28 @@ RSpec.describe "as an admin" do
       end
     end
 
-    # it "I see the date each user registered beside their name" do
-    #
-    # end
-    #
-    # it "I see an 'Upgrade to Merchant' button beside each user name" do
-    #
-    # end
+    it "I see the date each user registered beside their name" do
+      visit admin_users_path
+
+      within ".all-default-users" do
+        within ".default-user-#{@user_1.id}" do
+          expect(page).to have_content("Joined: #{@user_1.created_at}")
+        end
+
+        within ".default-user-#{@user_2.id}" do
+          expect(page).to have_content("Joined: #{@user_2.created_at}")
+        end
+
+        within ".default-user-#{@user_3.id}" do
+          expect(page).to have_content("Joined: #{@user_3.created_at}")
+        end
+
+        within ".default-user-#{@user_4.id}" do
+          expect(page).to have_content("Joined: #{@user_4.created_at}")
+        end
+      end
+    end
+
+    it "I see an 'Upgrade to Merchant' button beside each user name"
   end
 end
