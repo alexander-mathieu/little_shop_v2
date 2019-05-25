cd4768236dab976f4b8bbcb6fb0ce2ab6a9a46d6
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-05-25 09:41:52 -0600

    Add admin_user_path to routes

--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,6 @@ Rails.application.routes.draw do
   namespace :admin do
     get '/dashboard', to: 'dashboard#index'
 
-    resources :users, only: [:index]
+    resources :users, only: [:index, :show]
   end
 end
