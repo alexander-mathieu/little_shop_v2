fb2847987572c096216bc6a1af1ed7213ddb5635
Patrick Duvall <duvallp208@gmail.com>
2019-05-28 16:27:01 -0600

    Add spec for second log in correct after 1st wrong

--- spec/features/session/login_spec.rb
+++ spec/features/session/login_spec.rb
@@ -35,5 +35,20 @@ RSpec.describe "As a visitor," do
       expect(current_path).to eq(login_path)
       expect(page).to have_content("Some of your information isn't correct.")
     end
+
+    it "doesnt show a flash If I log in wrong then right" do
+      user = User.create!(email: "bob@bob.com", active: true, password: "124355",
+        name: "bob", address:"123 bob st.", city: "bobton", state:"MA", zip: 28234)
+
+        visit root_path
+        click_on "Login"
+        fill_in "email", with: "aaaah"
+        fill_in "password", with: user.password
+        click_on "Log In"
+        fill_in "email", with: user.email
+        fill_in "password", with: user.password
+        click_on "Log In"
+        expect(page).to_not have_content("Some of your information isn't correct.")
+    end
   end
 end
