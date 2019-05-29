d9bdb2c1e41d66f0f112d3c546096535b375301c
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-05-29 13:42:48 -0600

    Refactor Admin Merchant Index spec, add spacing

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -1,10 +1,3 @@
-
-# I see all merchants in the system
-# Next to each merchant's name I see their city and state
-# The merchant's name is a link to their Merchant Dashboard at routes such as "/admin/merchants/5"
-# I see a "disable" button next to any merchants who are not yet disabled
-# I see an "enable" button next to any merchants whose accounts are disabled
-
 require 'rails_helper'
 
 describe "as an admin" do
@@ -18,61 +11,76 @@ describe "as an admin" do
       visit root_path
 
       click_on "Login"
+
       fill_in "email", with: @admin_1.email
       fill_in "password", with: @admin_1.password
+
       click_on "Log In"
     end
 
     it "it displays all merchants" do
       visit merchants_path
+
       within "#merchant-#{@merchant_1.id}" do
         expect(page).to have_link(@merchant_1.name)
         expect(page).to have_content(@merchant_1.city)
         expect(page).to have_content(@merchant_1.state)
         expect(page).to have_link("Disable")
       end
+
       within "#merchant-#{@merchant_2.id}" do
         expect(page).to have_link(@merchant_2.name)
         expect(page).to have_content(@merchant_2.city)
         expect(page).to have_content(@merchant_2.state)
         expect(page).to have_link("Enable")
       end
-
     end
 
     it "lets an admin know if a merchant was enabled/disabled" do
       visit merchants_path
+
       within "#merchant-#{@merchant_2.id}" do
         click_link "Enable"
       end
-        expect(current_path).to eq(merchants_path)
-        expect(page).to have_content("Merchant has been enabled.")
-        @merchant_2.reload
-        expect(@merchant_2.active).to eq(true)
-        within "#merchant-#{@merchant_2.id}" do
-          click_link "Disable"
-        end
-        expect(current_path).to eq(merchants_path)
-        expect(page).to have_content("Merchant has been disabled.")
-        expect(User.find(@merchant_2.id).active).to eq(false)
+
+      expect(current_path).to eq(merchants_path)
+      expect(page).to have_content("Merchant has been enabled.")
+
+      @merchant_2.reload
+
+      expect(@merchant_2.active).to eq(true)
+
+      within "#merchant-#{@merchant_2.id}" do
+        click_link "Disable"
+      end
+
+      expect(current_path).to eq(merchants_path)
+      expect(page).to have_content("Merchant has been disabled.")
+      expect(User.find(@merchant_2.id).active).to eq(false)
     end
 
     it "only lets enabled users log in" do
       click_on "Logout"
+
       click_on "Login"
+
       fill_in "email", with: @merchant_1.email
       fill_in "password", with: @merchant_1.password
+
       click_on "Log In"
-      expect(current_path).to eq(dashboard_path(@merchant_1))
+
+      expect(current_path).to eq(dashboard_path)
+
       click_on "Logout"
+
       click_on "Login"
+
       fill_in "email", with: @merchant_2.email
       fill_in "password", with: @merchant_2.password
+
       click_on "Log In"
+
       expect(current_path).to eq(login_path)
-
     end
-
   end
-
 end
