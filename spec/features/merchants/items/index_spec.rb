9b03147b196e3f4900f50d94798e20acfddc5496
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-05-29 13:17:47 -0600

    Add Merchant Items index spec with initial test

---
+++ spec/features/merchants/items/index_spec.rb
@@ -0,0 +1,65 @@
+require 'rails_helper'
+
+RSpec.describe "as a merchant" do
+  describe "when I visit my item index page" do
+    before :each do
+      @merchant = User.create!(email: "merchant@gmail.com", role: 1, active: true, name: "Merchant", address: "Merchant Address", city: "Merchant City", state: "Merchant State", zip: "22345", password: "123456")
+
+      @item_1 = @merchant.items.create!(name: "Item 1", active: true, price: 1.00, description: "Item 1 Description", image: "https://tradersofafrica.com/img/no-product-photo.jpg", inventory: 10)
+      @item_2 = @merchant.items.create!(name: "Item 2", active: false, price: 2.00, description: "Item 2 Description", image: "https://tradersofafrica.com/img/no-product-photo.jpg", inventory: 15)
+      @item_3 = @merchant.items.create!(name: "Item 3", active: true, price: 3.00, description: "Item 3 Description", image: "https://tradersofafrica.com/img/no-product-photo.jpg", inventory: 20)
+      @item_4 = @merchant.items.create!(name: "Item 4", active: true, price: 4.00, description: "Item 4 Description", image: "https://tradersofafrica.com/img/no-product-photo.jpg", inventory: 25)
+
+      visit root_path
+
+      click_link "Login"
+
+      fill_in 'email', with: @merchant.email
+      fill_in 'password', with: @merchant.password
+
+      click_button "Log In"
+
+      click_link "Dashboard"
+
+      click_link "View All Items"
+    end
+
+    it "it displays all my items" do
+      within "#item-#{@item_1.id}" do
+        expect(page).to have_content(@item_1.name)
+        expect(page).to have_content(@item_1.inventory)
+        expect(page).to have_content("Edit")
+        expect(page).to have_content("Disable")
+        expect(page).to_not have_content("Enable")
+        expect(page).to have_content("Delete")
+      end
+
+      within "#item-#{@item_2.id}" do
+        expect(page).to have_content(@item_2.name)
+        expect(page).to have_content(@item_2.inventory)
+        expect(page).to have_content("Edit")
+        expect(page).to_not have_content("Disable")
+        expect(page).to have_content("Enable")
+        expect(page).to have_content("Delete")
+      end
+      save_and_open_page
+      within "#item-#{@item_3.id}" do
+        expect(page).to have_content(@item_3.name)
+        expect(page).to have_content(@item_3.inventory)
+        expect(page).to have_content("Edit")
+        expect(page).to have_content("Disable")
+        expect(page).to_not have_content("Enable")
+        expect(page).to have_content("Delete")
+      end
+
+      within "#item-#{@item_4.id}" do
+        expect(page).to have_content(@item_4.name)
+        expect(page).to have_content(@item_4.inventory)
+        expect(page).to have_content("Edit")
+        expect(page).to have_content("Disable")
+        expect(page).to_not have_content("Enable")
+        expect(page).to have_content("Delete")
+      end
+    end
+  end
+end
